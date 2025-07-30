c85a24dc9629550304a49bb614dccc4dadb7a68d
lrs2187 <liurongshuo2022@outlook.com>
2025-07-30 21:49:16 +0800

    feat: 添加了 ai 写的 BlockMetadata

---
+++ SuperAutoIsland.Interface/Shared/BlockMetadata.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SuperAutoIsland.Interface.Shared;
+
+// 类型定义（与之前相同）
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum MetaType
+{
+    dummy,
+    text,
+    number,
+    boolean,
+    dropdown,
+    checkbox
+}
+
+public abstract class MetaArgsBase
+{
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("type")]
+    public required MetaType Type { get; set; }
+}
+
+public class CommonMetaArgs : MetaArgsBase
+{
+    // 没有额外属性
+}
+
+public class DropDownMetaArgs : MetaArgsBase
+{
+    [JsonPropertyName("options")]
+    public required List<Tuple<string, string>> Options { get; set; }
+}
+
+public class CheckboxMetaArgs : MetaArgsBase
+{
+    [JsonPropertyName("defaultValue")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? DefaultValue { get; set; }
+}
+
+public class BlockMetadata
+{
+    [JsonPropertyName("id")]
+    public required string Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("icon")]
+    public required Tuple<string, string> Icon { get; set; }
+
+    [JsonPropertyName("args")]
+    public required Dictionary<string, MetaArgsBase> Args { get; set; }
+
+    [JsonPropertyName("inlineBlock")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? InlineBlock { get; set; }
+
+    [JsonPropertyName("inlineField")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? InlineField { get; set; }
+
+    [JsonPropertyName("dropdownUseNumbers")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? DropdownUseNumbers { get; set; }
+
+    [JsonPropertyName("isRule")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? IsRule { get; set; }
+}
---
+++ SuperautoIsland/Shared/MetadataToJson.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SuperAutoIsland.Interface.Shared;
+
+namespace SuperAutoIsland.Shared;
+
+public static class MetadataGenerator
+{
+    public static string GenerateMetaBlock(BlockMetadata data)
+    {
+        // 配置 JSON 序列化选项
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Converters = { new MetaArgsConverter() } // 添加自定义转换器
+        };
+
+        // 添加枚举字符串转换器
+        options.Converters.Add(new JsonStringEnumConverter());
+
+        try
+        {
+            // 序列化对象
+            return JsonSerializer.Serialize(data, options);
+        }
+        catch (JsonException ex)
+        {
+            throw new InvalidOperationException("Failed to generate metadata JSON", ex);
+        }
+    }
+}
+
+// 自定义转换器处理多态类型
+public class MetaArgsConverter : JsonConverter<MetaArgsBase>
+{
+    public override MetaArgsBase Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException("This converter is for serialization only");
+    }
+
+    public override void Write(Utf8JsonWriter writer, MetaArgsBase value, JsonSerializerOptions options)
+    {
+        // 根据实际类型进行序列化
+        switch (value)
+        {
+            case CommonMetaArgs common:
+                JsonSerializer.Serialize(writer, common, options);
+                break;
+                
+            case DropDownMetaArgs dropdown:
+                JsonSerializer.Serialize(writer, dropdown, options);
+                break;
+                
+            case CheckboxMetaArgs checkbox:
+                JsonSerializer.Serialize(writer, checkbox, options);
+                break;
+                
+            default:
+                throw new JsonException($"Unknown MetaArgs type: {value.GetType()}");
+        }
+    }
+}
+
+public class TupleConverter : JsonConverter<ValueTuple<string, string>>
+{
+    public override (string, string) Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+            throw new JsonException("Expected array start");
+        
+        reader.Read();
+        var item1 = reader.GetString()!;
+        reader.Read();
+        var item2 = reader.GetString()!;
+        reader.Read(); // Consume end array
+        
+        return (item1, item2);
+    }
+
+    public override void Write(Utf8JsonWriter writer, (string, string) value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+        writer.WriteStringValue(value.Item1);
+        writer.WriteStringValue(value.Item2);
+        writer.WriteEndArray();
+    }
+}
