e3008886e75c2fe6209168c423ac30048f57321a
lrs2187 <liurongshuo2022@outlook.com>
2026-07-26 16:47:53 +0800

    feat: 解决 Blockly Procedure 不支持异步的问题

--- SuperAutoIsland.Frontend/src/blockly/index.ts
+++ SuperAutoIsland.Frontend/src/blockly/index.ts
@@ -1,5 +1,6 @@
 ﻿import * as Blockly from 'blockly';
-import { javascriptGenerator } from 'blockly/javascript';
+import { type JavascriptGenerator, Order, javascriptGenerator } from 'blockly/javascript';
+import type { Block } from 'blockly';
 import { toolbox } from './toolbox';
 import blocklyLangZhHans from './langs/zh-hans';
 import { preSetupCategory, postSetupCategory } from './utils/quickSetup';
@@ -26,7 +27,83 @@ window.extraBlocks = JSON.parse(data.blocksString) as Record<string, Record<'rul
 window.saiWS = ws;
 window.saiWaitMessage = wsWaitMessage;
 
+// modify procedures interaction
+javascriptGenerator.forBlock['procedures_defreturn'] = function (block: Block, generator: JavascriptGenerator) {
+    // Define a procedure with a return value.
+    const funcName = generator.getProcedureName(block.getFieldValue('NAME'));
+    let xfix1 = '';
+    if (generator.STATEMENT_PREFIX) {
+        xfix1 += generator.injectId(generator.STATEMENT_PREFIX, block);
+    }
+    if (generator.STATEMENT_SUFFIX) {
+        xfix1 += generator.injectId(generator.STATEMENT_SUFFIX, block);
+    }
+    if (xfix1) {
+        xfix1 = generator.prefixLines(xfix1, generator.INDENT);
+    }
+    let loopTrap = '';
+    if (generator.INFINITE_LOOP_TRAP) {
+        loopTrap = generator.prefixLines(generator.injectId(generator.INFINITE_LOOP_TRAP, block), generator.INDENT);
+    }
+    let branch = '';
+    if (block.getInput('STACK')) {
+        // The 'procedures_defreturn' block might not have a STACK input.
+        branch = generator.statementToCode(block, 'STACK');
+    }
+    let returnValue = '';
+    if (block.getInput('RETURN')) {
+        // The 'procedures_defnoreturn' block (which shares this code)
+        // does not have a RETURN input.
+        returnValue = generator.valueToCode(block, 'RETURN', Order.NONE) || '';
+    }
+    let xfix2 = '';
+    if (branch && returnValue) {
+        // After executing the function body, revisit this block for the return.
+        xfix2 = xfix1;
+    }
+    if (returnValue) {
+        returnValue = generator.INDENT + 'return ' + returnValue + ';\n';
+    }
+    const args = [];
+    const variables = block.getVarModels();
+    for (let i = 0; i < variables.length; i++) {
+        args[i] = generator.getVariableName(variables[i].getId());
+    }
+    let code =
+        'async function ' +
+        funcName +
+        '(' +
+        args.join(', ') +
+        ') {\n' +
+        xfix1 +
+        loopTrap +
+        branch +
+        xfix2 +
+        returnValue +
+        '}';
+    code = generator.scrub_(block, code);
+    // Add % so as not to collide with helper functions in definitions list.
+    // CodeGenerator declaring .definitions protected.
+    (generator as any).definitions_['%' + funcName] = code;
+    return null;
+};
 
+javascriptGenerator.forBlock['procedures_defnoreturn'] = javascriptGenerator.forBlock['procedures_defreturn'];
+
+javascriptGenerator.forBlock['procedures_callreturn'] = function (
+    block: Block,
+    generator: JavascriptGenerator,
+): [string, Order] {
+    // Call a procedure with a return value.
+    const funcName = generator.getProcedureName(block.getFieldValue('NAME'));
+    const args = [];
+    const variables = block.getVarModels();
+    for (let i = 0; i < variables.length; i++) {
+        args[i] = generator.valueToCode(block, 'ARG' + i, Order.NONE) || 'null';
+    }
+    const code = 'await ' + funcName + '(' + args.join(', ') + ')';
+    return [code, Order.FUNCTION_CALL];
+};
 
 preSetupCategory('ClassIsland');
 
@@ -48,7 +125,7 @@ for (let pluginName in window.extraBlocks) {
     let data = window.extraBlocks[pluginName].data;
 
     if (rules != undefined && rules.length != 0) {
-        addLabel("规则");
+        addLabel('规则');
 
         for (let block of rules) {
             await addMetaBlock('rule', block);
@@ -56,7 +133,7 @@ for (let pluginName in window.extraBlocks) {
     }
 
     if (actions != undefined && actions.length != 0) {
-        addLabel("行动");
+        addLabel('行动');
 
         for (let block of actions) {
             await addMetaBlock('action', block);
@@ -82,8 +159,8 @@ postSetupCategory();
 Blockly.setLocale(blocklyLangZhHans);
 Blockly.ContextMenuItems.registerCommentOptions();
 
-let projectUuid = new URLSearchParams(location.search).get("id") || "";
-if (projectUuid === "") {
+let projectUuid = new URLSearchParams(location.search).get('id') || '';
+if (projectUuid === '') {
     location.href = `/?id=${uuid()}`;
 }
 
@@ -174,15 +251,16 @@ export const saveCode = async (workspace: Blockly.Workspace = window.workspace) 
     });
     console.log(code);
 
-    await wsWaitMessage(ws, { type: "save",
+    await wsWaitMessage(ws, {
+        type: 'save',
         data: {
-            type: "blocklyAction",
+            type: 'blocklyAction',
             guid: projectUuid,
             workspace: JSON.stringify(Blockly.serialization.workspaces.save(workspace)),
-            code: code
-        }
-    })
-}
+            code: code,
+        },
+    });
+};
 window.saveCode = saveCode;
 
 /**
@@ -202,9 +280,9 @@ export const injectBlockly = async (dom: HTMLElement) => {
     Reflect.set(window, 'Blockly', Blockly);
 
     if (workspace) {
-        let data: { type: string; workspace: string; guid: string; };
+        let data: { type: string; workspace: string; guid: string };
         try {
-            data = await wsWaitMessage<{ workspace: string; guid: string; }>(ws, { type: "load", guid: projectUuid });
+            data = await wsWaitMessage<{ workspace: string; guid: string }>(ws, { type: 'load', guid: projectUuid });
             if (data.guid !== projectUuid) {
                 location.href = `/?id=${data.guid}`;
             }
@@ -212,8 +290,8 @@ export const injectBlockly = async (dom: HTMLElement) => {
             Blockly.Events.disable();
             Blockly.serialization.workspaces.load(JSON.parse(data.workspace), workspace, undefined);
             Blockly.Events.enable();
-        } catch {
-            console.log("Load Failed!")
+        } catch (error) {
+            console.error('Load Failed!', error);
         }
 
         return workspace;
